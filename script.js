9b88885d7b8d89da1b586caa919ff9c9cd59332c
Gengis <gengis.gutierrez.c@gmail.com>
2024-02-12 23:56:37 -0500

    Some changes

--- script.js
+++ script.js
@@ -4,15 +4,35 @@ const buttonYes = document.querySelector('#yes')
 let fontSize = 2
 
 let messages = [
-  'Estas segurx?',
-  'Piensalo bien',
+  'Ey, porque dices que no?',
+  'Ey ey ey pequeña no digas eso ... No digas eso ... Eres perfecta',
   'Piensalo muy bien',
   'Piensalo',
   'Mira el otro botón'
 ]
 
+var myAudio = document.getElementById("myAudio");
+var playPauseButton = document.getElementById("playPauseButton");
+var volumeControl = document.getElementById("volumeControl");
+
+function togglePlayPause() {
+  if (myAudio.paused) {
+      myAudio.play();
+      playPauseButton.src = "assets/pause.png";
+  } else {
+      myAudio.pause();
+      playPauseButton.src = "assets/play.png";
+  }
+}
+
+function changeVolume() {
+  myAudio.volume = volumeControl.value;
+}
+
+
+
 buttonNo.addEventListener('click', () => {
-  fontSize = fontSize + .5
+  fontSize = fontSize + .2
   buttonYes.style.fontSize = `${fontSize}rem`
 
   const indexRandom = Math.floor(Math.random() * messages.length)
